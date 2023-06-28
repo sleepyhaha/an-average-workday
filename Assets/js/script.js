5005799be6fe653a4e45d8be697d39563919614f
sleepyhaha <131776408+sleepyhaha@users.noreply.github.com>
2023-06-28 22:19:03 +1000

    set time

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -4,19 +4,57 @@
 // function? How can DOM traversal be used to get the "hour-x" id of the
 // time-block containing the button that was clicked? How might the id be
 // useful when saving the description in local storage?
-//
-// TODO: Add code to apply the past, present, or future class to each time
-// block by comparing the id to the current hour. HINTS: How can the id
-// attribute of each time-block be used to conditionally add or remove the
-// past, present, and future classes? How can Day.js be used to get the
-// current hour in 24-hour time?
-//
+
+const saveButtonHandler = function () {
+  let description = $(".saveBtn").siblings(".description").val();
+  let timeOfDay = $(".time-block").attr("id");
+  console.log(description);
+  localStorage.setItem(timeOfDay, description);
+};
+
+let timeCompare = function () {
+  let currentTime = dayjs().hour();
+
+  $(".time-block").each(function () {
+    let divHour = +$(this).attr("id");
+
+    if (divHour < currentTime) {
+      $(this).removeClass("future", "present");
+      $(this).addClass("past");
+    }
+
+    if (divHour === currentTime) {
+      $(this).removeClass("future", "past");
+      $(this).addClass("present");
+    }
+
+    if (divHour > currentTime) {
+      $(this).removeClass("past", "present");
+      $(this).addClass("future");
+    }
+  });
+};
+
 // TODO: Add code to get any user input that was saved in localStorage and set
 // the values of the corresponding textarea elements. HINT: How can the id
 // attribute of each time-block be used to do this?
-//
+
+$(".btn").on("click", saveButtonHandler);
+
 // TODO: Add code to display the current date in the header of the page.
 
 $("#currentDay")
   .append("<h2></h2>")
   .text(dayjs().format("dddd, D MMM. h:mm A"));
+
+timeCompare();
+
+// function renderText() {
+//   $(".time-block").each(function () {
+//     for (const taskText of $(".time-block textarea")) {
+//       taskText.text(localStorage.getItem(description));
+//     }
+//   });
+// }
+
+// renderText();
